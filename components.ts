55f85cd8ec8a9f38630ca9feb8d165b7354979b8
louix <28579246+louix@users.noreply.github.com>
2021-08-30 14:09:16 +0100

    refactor to simpler components

--- components.ts
+++ components.ts
@@ -1,24 +1,11 @@
-import { some } from "./types/option";
-import type { Option } from "./types/option";
+import type { OptionalArrayValuesMap, OptionalValues, UnionKeys } from "./types/component";
 
-type ComponentArray<K extends string, V> = { [key in K]: Array<Option<V>>}
-export type ComponentSingle<K extends string, V> = { [key in K]: Option<V> }
+type Name = { name: string };
+type Position = { position: { x: number, y: number } };
+type Renderable = { renderable: void };
 
-type NameData = string;
-type NameMap = ComponentArray<"name", NameData>
-type NameComponent = ComponentSingle<"name", NameData>
-export const NameComponent = (data: NameData): NameComponent => ({ name: some(data) })
+export type Component = Name | Position | Renderable;
 
-type PositionData = { x: number, y: number };
-type PositionMap = ComponentArray<"position", PositionData>
-type PositionComponent = ComponentSingle<"position", PositionData>
-export const PositionComponent = (data: PositionData): PositionComponent => ({ position: some(data) });
-
-type RenderableData = void;
-type RenderableMap = ComponentArray<"renderable", RenderableData>
-type RenderableComponent = ComponentSingle<"renderable", RenderableData>
-export const RenderableComponent = (): RenderableComponent => ({ renderable: some(undefined) });
-
-export type Component = NameComponent | PositionComponent | RenderableComponent
-export type ComponentMap = NameMap & PositionMap & RenderableMap
-export type ComponentKind = keyof ComponentMap;
+export type ComponentMap = OptionalArrayValuesMap<Component>;
+export type ComponentOption = OptionalValues<Component>;
+export type ComponentKind = UnionKeys<Component>;
