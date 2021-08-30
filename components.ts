23fe3cc09735592128133464ecc454f469eeedf9
louix <louix@otsuka.re>
2021-08-30 15:23:03 +0100

    documentation

--- components.ts
+++ components.ts
@@ -1,11 +1,14 @@
 import type { OptionalArrayValuesMap, OptionalValues, UnionKeys, UnionValues } from "./types/component";
 
+// Add new components here
 type Name = { name: string };
 type Position = { position: { x: number, y: number } };
 type Renderable = { renderable: void };
 
+// Make sure to add your components to the below union
 export type Component = Name | Position | Renderable;
 
+// Dynamic Component Types
 export type ComponentMap = OptionalArrayValuesMap<Component>;
 export type ComponentOption = OptionalValues<Component>;
 export type ComponentKind = UnionKeys<Component>;
--- world.ts
+++ world.ts
@@ -14,7 +14,12 @@ export class World {
         renderable: []
       }
     }
-
+    /**
+     * Create and return a new entity
+     * @example
+     * const world = new World();
+     * const myEntity = world.newEntity();
+     */
     public newEntity (): Entity {
         Object.keys(this.#components).forEach((k) => {
             this.#components.name
@@ -22,12 +27,24 @@ export class World {
       })
       return this.#entityCount++
     }
-
+    /**
+     * Add a component to an entity
+     * @example
+     * const world = new World();
+     * const myEntity = world.newEntity();
+     * world.addComponentToEntity(myEntity, {name: "entity"});
+     */
     public addComponentToEntity (entity: Entity, component: Component): void {
       const [key, value] = Object.entries(component)[0]
       this.#components[key as ComponentKind][entity] = value
     }
 
+    /**
+     * Get a list of entities by component.
+     * Usually used in a system to get the components it needs to act on.
+     * @example
+     * const entities = world.getEntitiesByComponentKinds("position", "velocity")
+     */
     public getEntitiesByComponentKinds (...components: ComponentKind[]): Entity[] {
       const entities = []
       for (let i = 0; i < this.#entityCount; i++) {
@@ -38,6 +55,14 @@ export class World {
       return entities
     }
 
+    /**
+     * Get the data associated with an entity for a given component
+     * @example
+     * const world = new World();
+     * const myEntity = world.newEntity();
+     * world.addComponentToEntity(myEntity, {name: "entity"});
+     * world.getComponentDataForEntity(myEntity, "name");
+     */
     public getComponentDataForEntity(entity: Entity, component: ComponentKind) {
         const data = this.#components[component][entity];
         if (isSome<ComponentData>(data)) {
