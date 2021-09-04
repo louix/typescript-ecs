773fd7b1817257440ffc17594e37db7f5658ed41
louix <louix@otsuka.re>
2021-09-04 10:22:15 +0100

    reordered files

--- systems.ts
+++ systems.ts
@@ -1,1 +1,17 @@
-type System = (delta: number, time: number) => void;
+import { Entity } from "./types/ecs/entity";
+import { isSome, unwrap } from "./types/ecs/option";
+import { System } from "./types/ecs/system";
+import { World } from "./world";
+
+const VELOCITY = { x: 0.2, y: 0.3 };
+
+export const MovableSystem: System = (delta: number, time: number) => (world: World) => {
+    world.getEntitiesByComponentKinds("position").forEach((entity: Entity) => {
+        const data = world.getComponentDataForEntity(entity, "position");
+        if (isSome(data)) {
+            const positionData = unwrap(data);
+            positionData.x += VELOCITY.x * delta
+            positionData.y += VELOCITY.y * delta
+        }
+    })
+}
