b052a12bd68563ae68fe63737b015d9132c3ca24
louix <28579246+louix@users.noreply.github.com>
2021-09-04 10:22:38 +0100

    finished basic implementation

--- main.ts
+++ main.ts
@@ -1,25 +1,35 @@
+import { MovableSystem } from "./systems";
 import { World } from "./world";
 
 console.time("setup");
 
-export const WORLD = new World();
+const world = new World();
 
-const entities = new Array(1000).fill(0).map((_) => WORLD.newEntity());
+const entities = new Array(10).fill(0).map((_) => world.newEntity());
 entities.forEach((entity) => {
-    if (Math.random() > 0.5) {
-        WORLD.addComponentToEntity(entity, { position: { x: 0, y: 0 } })
+    if (true) {
+        world.addComponentToEntity(entity, { position: { x: 0, y: 0 } })
     }
     if (Math.random() > 0.5) {
-        WORLD.addComponentToEntity(entity, { name: `entity${entity}` })
+        world.addComponentToEntity(entity, { name: `entity${entity}` })
     }
     if (Math.random() > 0.5) {
-        WORLD.addComponentToEntity(entity, {renderable: undefined })
+        world.addComponentToEntity(entity, {renderable: undefined })
     }
 })
 
 console.timeEnd("setup");
-console.time("lookup");
 
-console.log(JSON.stringify(WORLD.getEntitiesByComponentKinds("position")));
+world.registerSystem(MovableSystem);
 
-console.timeEnd("lookup");
+const run = (previousTime: number) => {
+    setTimeout(() => {
+        console.log("tick");
+        const time = performance.now();
+        const delta = time - previousTime;
+        world.run(delta, time);
+        run(time)
+    }, 1)
+}
+
+run(performance.now())
