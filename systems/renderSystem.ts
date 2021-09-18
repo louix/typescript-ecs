61d2e521240f25b4bab15d109b9c5587156315ce
louix <28579246+louix@users.noreply.github.com>
2021-09-18 13:15:38 +0100

    extract render system

--- systems/renderSystem.ts
+++ systems/renderSystem.ts
@@ -1,35 +1,26 @@
+import type { Entity } from "../types/game/entity";
 import { unsafeUnwrap } from "../types/game/option";
-import type { System } from "../types/game/system";
-import type { Coordinates2D } from "../types/space";
 import type { World } from "../world";
 
-const drawImage = (ctx: CanvasRenderingContext2D, source: string, position: { x: number, y: number }) => {
-    const image = new Image();
-    image.src = source;
-    ctx.drawImage(image, position.x, position.y, image.width, image.height);
+export interface EntityData {
+    id: Entity,
+    shape: ReturnType<typeof getShape>
+    position: ReturnType<typeof getPosition>
 }
 
-interface RenderSystemEnv {
-    ctx: CanvasRenderingContext2D,
-    worldSize: Coordinates2D,
-    images: {
-        moppie: string,
-        flower: string,
-        flowerless: string
-    }
+const getShape = (entity: Entity, world: World) => {
+    return unsafeUnwrap(world.getComponentDataForEntity(entity, "shape"));
 }
 
-export const mkRenderSystem = ({ ctx, worldSize, images }: RenderSystemEnv): System =>
-    () => (world: World) => {
-        ctx.fillStyle = "#019120";
-        ctx.fillRect(0, 0, worldSize.x, worldSize.y);
-        world.getEntitiesByComponentKinds("renderable", "shape", "position").forEach((entity) => {
-            const shape = unsafeUnwrap(world.getComponentDataForEntity(entity, "shape"));
-            const position = unsafeUnwrap(world.getComponentDataForEntity(entity, "position"));
-            switch (shape) {
-                case "moppie": return drawImage(ctx, images.moppie, position);
-                case "flower": return drawImage(ctx, images.flower, position);
-                case "flowerless": return drawImage(ctx, images.flowerless, position);
-            }
+const getPosition = (entity: Entity, world: World) => {
+    return unsafeUnwrap(world.getComponentDataForEntity(entity, "position"));
+}
+
+export const mkRenderSystem = () => // TODO: type this better?
+    (world: World): Array<EntityData> => {
+        return world.getEntitiesByComponentKinds("renderable", "shape", "position").map((entity) => {
+            const shape = getShape(entity, world);
+            const position = getPosition(entity, world);
+            return { id: entity, shape, position };
         })
     }
