5c569019a06565fcb8420ca8474a5b11101ea45b
louix <28579246+louix@users.noreply.github.com>
2023-04-10 00:14:41 +0100

    rm eslint junk

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,9 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-    // browser: true
-  },
-  extends: [
-    'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['standard-with-typescript'],
   parserOptions: {
-    ecmaVersion: 12,
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {
-    "@typescript-eslint/space-before-function-paren": false
-  }
+  plugins: [],
+  rules: {}
 }
